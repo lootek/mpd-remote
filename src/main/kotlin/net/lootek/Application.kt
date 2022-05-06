0dacd409530f231c67952d9e0497595998383155
lootek <piotr.lucinski@solarwinds.com>
2022-05-06 19:21:17 +0200

    init with ktor boilerplate by IntelliJ

---
+++ src/main/kotlin/net/lootek/Application.kt
@@ -0,0 +1,13 @@
+package net.lootek
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import net.lootek.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSecurity()
+        configureTemplating()
+    }.start(wait = true)
+}
